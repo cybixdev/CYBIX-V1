6842c8448434e872ee0290a6a7fd6ea0f4f6192a
cybixdev <nexoratechn@gmail.com>
2025-09-03 13:58:22 +0200

    update

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   botToken: process.env.BOT_TOKEN,
   ownerId: process.env.OWNER_ID,
   port: process.env.PORT || 3000,
-  bannerUrl: 'https://files.catbox.moe/wf749g.jpg',
+  bannerUrl: 'https://files.catbox.moe/j0v4no.jpg',
   whatsappChannel: 'https://whatsapp.com/channel/0029VbB8svo65yD8WDtzwd0X',
   telegramChannel: 'https://t.me/cybixtech',
   developerContact: 'https://t.me/cybixdev',
